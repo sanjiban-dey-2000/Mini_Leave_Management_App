6224a9a1e8be0fc9a690d110efb4a0ef8df2f985
Sanjiban Dey <sanjibandey2000@gmail.com>
2025-08-18 09:41:01 +0530

    Leave model created

---
+++ backend/src/models/leaveModel.js
@@ -0,0 +1,60 @@
+const mongoose=require('mongoose');
+
+const leaveSchema=new mongoose.Schema({
+    employeeId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Employee",
+        require:true,
+    },
+    leaveType:{
+        type:String,
+        enum:["Sick Leave","Casual Leave","Paid Leave","Unpaid Leave"],
+        require:true,
+    },
+    startDate:{
+        type:Date,
+        require:true,
+    },
+    endDate:{
+        type:Date,
+        require:true,
+    },
+    numberOfDays:{
+        type:String,
+        require:true,
+    },
+    reason:{
+        type:String,
+        require:true,
+    },
+    status:{
+        type:String,
+        enum:["Pending","Approved","Rejected","Cancelled"],
+        default:"Pending",
+    },
+    appliedOn:{
+        type:Date,
+        default:Date.now,
+    },
+    approvedBy:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Admin",
+    },
+    leaveBalanceBefore:{
+        type:Number,
+    },
+    leaveBalanceAfter:{
+        type:Number,
+    },
+    documents:[
+        {
+            type:String,
+        }
+    ],
+},{
+    timestamps:true,
+});
+
+const Leave=mongoose.model("Leaves",leaveSchema);
+
+module.exports=Leave;
